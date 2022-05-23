1e27776f9379d23d2d7ff4d051645e2ddcdd76f8
Shivamyadav07 <shivamvnsindia@gmail.com>
2022-05-23 16:29:59 +0530

    update

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import styles from "./addTask.module.css";
 
-const AddTask = ({tasklist,task,settask,settasklist }) => {
+const AddTask = ({ tasklist, task, settask, settasklist }) => {
 
   return (
     <div className={styles.todoForm}>
       <input data-cy="add-task-input" placeholder="Add Task...." type="text" onChange={(e) => { settask(e.target.value) }}></input>
       <button data-cy="add-task-button" onClick={() => {
         settasklist([...tasklist, task])
-        
+
       }
       }>add</button>
     </div>
--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./counter.module.css";
 
 const Counter = () => {
-  // sample value to be replaced
-  let count = 0;
-  // NOTE: do not delete `data-cy` key value pair
+  const [count, setcount] = useState(0);
+  const handlechange = (data) => {
+    if (count < 0 || count === 0) { }
+    else {
+      setcount(data)
+    }
+
+  }
   return (
     <div className={styles.counter}>
-      <button data-cy="task-counter-increment-button">incr</button>
+      <button data-cy="task-counter-increment-button" onClick={() => setcount(count + 1)} >incr</button>
       <span data-cy="task-counter-value">{count}</span>
-      <button data-cy="task-counter-decrement-button">decr</button>
+      <button data-cy="task-counter-decrement-button" onClick={() => handlechange(count - 1)} >decr</button>
     </div>
   );
 };
